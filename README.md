74986aeb59748268a70c7e83b639b06160906762
sabasm <smma1992@gmail.com>
2018-12-18 16:48:45 -0600

    front end deployed successfully to firebase

--- README.md
+++
@@ -1,7 +0,0 @@
-MicroTransactions Lab
-
-Its a web service platform where people can ask to be lend any incomes
-
-## Info
-
-later on
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-<h1>Micro-transactions app</h1>
-<h3>by sabás mendívil</h3>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
     );
   }

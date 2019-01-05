1acb297a2d41d95a716704561a270c2072fb5a79
sabasm <smma1992@gmail.com>
2019-01-04 21:17:07 -0600

    Ready signup messages and errors, methods ready to be copied to logins and forgotten data

--- frontend/src/components/auth/SignupForm.js
+++ frontend/src/components/auth/SignupForm.js
@@ -10,12 +10,15 @@ import {
 import {
     API_lookFor
 } from '../services/database'
+import { Link } from 'react-router-dom'
 
 class SignupForm extends Component {
 
     state = {
         user: {},
-        redirect: false
+        redirect: false,
+        error:false,
+        errorsPicker:{user1:false,user2:false,email1:false,email2:false,name:false,password:false}
     }
 
     renderRedirect = () => {
@@ -25,13 +28,9 @@ class SignupForm extends Component {
     }
 
     signup = (e) => {
-        const {
-            user
-        } = this.state
-        console.log(user)
+        const {user} = this.state
         signup(user)
             .then(r => {
-                console.log(r)
                 this.setState({
                     redirect: true
                 })
@@ -42,10 +41,11 @@ class SignupForm extends Component {
 
     lookFor = (lookingFor) => {
         return new Promise((resolve, reject) => {
-        let exist;
+        let exist=false;
             API_lookFor(lookingFor)
                 .then(r => {
-                    exist = r.exist
+                    exist=r.exist
+                    if (this.state.user === "") exist=false
                     resolve(exist)
                 }).catch(r => {
                     reject("Favor de llenar todos los datos")
@@ -57,17 +57,18 @@ class SignupForm extends Component {
         e.preventDefault()
         let check=0;
         const {user} = this.state
-        console.log(user)
 //CHECKS EN BASE DE DATOS
         //...CHECK 1
         //buscar usuario repetido
         let busqueda1 = new Promise ((resolve,reject)=>{
             this.lookFor({username: user.username})
             .then(r=>{
-            if (r){console.log("Este usuario ya está tomado")}else{
-               console.log("check no existe username")
-               check++
-               resolve()} 
+            if (r){
+            resolve(this.state.errorsPicker.user1=true)
+            } else{
+            check++
+            resolve(this.state.errorsPicker.user1=false)
+            }
             })
             .catch(e=>console.log(e))
         })
@@ -77,11 +78,14 @@ class SignupForm extends Component {
             this.lookFor({email: user.email})
             .then(r=>{
             if (r){
-                console.log("Este correo ya está en uso, si olvidaste tu contraseña da click aquí")
-            }else{
-                console.log("check correo no repetido")
+                console.log(user.email)
+                console.log("email existe= ",r)
+
+                resolve(this.state.errorsPicker.email1=true)
+               
+            } else {
                 check++
-                resolve()
+              resolve(this.state.errorsPicker.email1=false)
             }
             })
             .catch(e=>console.log(e))
@@ -91,9 +95,11 @@ class SignupForm extends Component {
             //...CHECK 3
         //checando longitud de usuario
         if (user.username.length <6 || user.username.length>12){
-            console.log("Tú usuario debe ser mayor de 6 caracteres y menor de 12 caracteres")
+            this.state.errorsPicker.user2=true
+            // console.log("Tu usuario debe ser mayor de 6 caracteres y menor de 12 caracteres")
         }else{
-            console.log("check longitud username")
+            // console.log("check longitud username")
+            this.state.errorsPicker.user2=false
             check++
         }
 
@@ -101,38 +107,37 @@ class SignupForm extends Component {
         //comprobando correo válido
         if(/(.+)@(.+){2,}\.(.+){2,}/.test(user.email)) {
             check++
-            console.log("check correo valido")
-        }else{
-            console.log("ingresa un email válido")}
+            this.state.errorsPicker.email2=false
+        }else{this.state.errorsPicker.email2=true}
         
         //...CHECK 5
         //comprobando que valor en el espacio de nombre
         if (user.name===undefined || user.name.length <6 ){
-            console.log("Ingresa un nombre válido")
+            this.state.errorsPicker.name=true
         }else{
-            console.log("check nombre valido")
+            this.state.errorsPicker.name=false
             check++
         }
         //...CHECK 6
         //comprobando contraseña
         if (user.password===undefined || !user.password.match(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([a-zA-Z0-9]{6,})$/) ){
-            console.log("Tu password debe ser mayor de 5 caracteres y contener por lo menos un número, una mayúscula y una minúscula")
+            this.state.errorsPicker.password=true
         }else{
-            console.log("check contraseña valida")
-            check++
+            this.state.errorsPicker.password=false
+        check++
         }
 
         Promise.all([busqueda1,busqueda2])
         .then(()=>{
-            if (check === 6) this.signup()
+            console.log(this.state.errorsPicker)
+            if(check === 6){ this.signup()}else{this.setState ({error:true})}
         })
+        .catch(r=>console.log("no entra al then pero si al catch del promise all"))
     }
 
 
     handleText = (e) => {
-        const {
-            user
-        } = this.state
+        const {user} = this.state
         const field = e.target.name
         user[field] = e.target.value
         this.setState({
@@ -153,28 +158,34 @@ class SignupForm extends Component {
             <input type = "text"
             name = "username"
             onChange = {this.handleText}/> 
+            { this.state.errorsPicker.user1 ? <small className="form-errors"><br/>Excelente nombre, pero ya está en uso, ¡elige uno mejor!</small>: null }
+            { this.state.errorsPicker.user2 ? <small className="form-errors"><br/>Tu usuario debe ser mayor de 6 caracteres y menor de 12 caracteres</small>: 
+            <small className="form-info"><br/>Tu usuario debe ser mayor de 6 caracteres y menor de 12 caracteres</small> }
             </label><br/> < br/>
             <label>
-            Nombre: < br />
+            Nombre completo: < br />
             <input type = "text"
             name = "name"
             onChange = {this.handleText}/> 
+            { this.state.errorsPicker.name ? <small className="form-errors"><br/>Ingresa tu nombre completo</small>:null}
             </label><br/> < br />
             <label>
             Email: < br/>
             <input type = "email"
             name = "email"
             onChange = {this.handleText}/> 
+
+            { this.state.errorsPicker.email1 ? <small className="form-errors"><br/>Este correo ya está en uso, si olvidaste tu contraseña da click <Link to="/">aquí</Link>.</small>: null }
+            { this.state.errorsPicker.email2 ? <small className="form-errors"><br/>Ingresa un email válido</small>: null }
             </label><br/> < br />
+
+
             <label>
-            Password: < br />
-            <
-            input type = "password"
-            name = "password"
-            onChange = {
-                this.handleText
-            }
-            /> </label><br/ > < br />
+            Contraseña: < br />
+            <input type = "password" name = "password" onChange = {this.handleText}/> 
+            {this.state.errorsPicker.password ? <small className="form-errors"><br/>Tu contraseña debe ser mayor de 5 caracteres y contener por lo menos un número, una mayúscula y una minúscula</small>: 
+            <small className="form-info"><br/>Tu contraseña debe ser mayor de 5 caracteres y contener por lo menos un número, una mayúscula y una minúscula</small> }
+            </label><br/ > < br />
             <button type = "submit" > Registrate </button> </form> </label> </div>
         )
     }

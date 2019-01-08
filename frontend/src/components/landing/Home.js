3afbebe117aca24d633139cd5d4d5f7a1e5ed8fb
sabasm <smma1992@gmail.com>
2019-01-08 15:37:33 -0600

    Demo images updated

--- frontend/src/components/landing/Home.js
+++ frontend/src/components/landing/Home.js
@@ -46,26 +46,34 @@ login=(e)=>{
     <div className="landing-info">
     <div className="landing-info-cards">
     <section>
-    <img src="" alt="entrepreneur-logo"/>
-        <h2>Estudiantes empresarios</h2>
+    <img src="https://image.flaticon.com/icons/svg/201/201818.svg" height="120" width="120" alt="entrepreneur-logo"/>
+        <b>
+        <h2>Estudiantes emprendedores</h2>
         <p>Queremos apoyar a los estudiantes en apuros y los nuevos empresarios que busquen mejorar la sociedad</p>
+        </b>
         </section>
         <section>
-      <img src="" alt="micro-empresas-logo"/>
+      <img src="https://image.flaticon.com/icons/svg/1055/1055646.svg" height="120" width="120" alt="micro-empresas-logo"/>
+        <b>
         <h2>Micro-empresas</h2>
         <p>Perfecto para personas trabajadoras que necesitan un ingreso extra para solucionar situaciones de inmediato</p>
+        </b>
         </section>
         <section>
-      <img src="" alt="social-logo"/>
+          <b>
+      <img src="https://image.flaticon.com/icons/svg/1049/1049867.svg" height="120" width="120" alt="social-logo"/>
         <h2>Social</h2>
         <p>Invita a tus amigos y conocidos, entre más invitados tengas, ¡tu monto de prestamo subirá proporcionalmente!</p>
+        </b>
         </section>
     </div>
       <div className="landing-info-invest"> 
-      <img src="" alt="investment-logo"/>
+      <img src="https://image.flaticon.com/icons/svg/877/877627.svg" height="120" width="120" alt="investment-logo"/>
+        <b>
         <h3>¡Invierte con nosotros!</h3>
         <h2>¿Que te parece un ingreso extra?</h2>
         <p>Cualquier usuario puede convertirse en inversor con sólo 100 pesos, gana hasta el 3.5% de tu inversión, y recibe un descuento de 2% en tus préstamos</p>
+        </b>
         </div>
     </div>
       </div>
--- frontend/src/components/services/navbar.js
+++ frontend/src/components/services/navbar.js
@@ -23,7 +23,7 @@ logout=(e)=>{
     return (
       <nav className="nav-bar">
       <section className="nav-bar-logo">
-        <NavLink to="/" className="nav-inactive"><img src="" alt="confiadoz-logo"/></NavLink>
+        <NavLink to="/" className="nav-inactive"><img src="https://image.flaticon.com/icons/svg/1155/1155253.svg" height="32" width="32" alt="confiadoz-logo"/></NavLink>
         </section>
       <section className="nav-bar-links-container">
         {/* <NavLink className="nav-links nav-inactive" to="/auth/login" activeClassName="nav-selected"><span>Inicia sesión</span></NavLink> */}
